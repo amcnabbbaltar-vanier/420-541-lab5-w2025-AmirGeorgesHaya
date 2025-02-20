f0589809c7b6e57c81a81276811d3d0a770047ca
AmirGeorgesHaya <amirgeorges.haya@icloud.com>
2025-02-19 21:41:55 -0500

    page 29 number 3

--- Assets/Scripts/PlayerHitBoxManagerScript.cs
+++ Assets/Scripts/PlayerHitBoxManagerScript.cs
@@ -2,20 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerHitBoxManager : MonoBehaviour
+public class PlayerHitBoxManagerScript : MonoBehaviour
 {
+        public Collider[] attackColliders;
     // Start is called before the first frame update
-    public Collider[] attackColliders;
     void Start()
     {
         foreach( Collider attackCollider in attackColliders)
         {
             attackCollider.enabled = false; // Disable collider at start
         }
-       
+
     }
 
-    public void EnableHitbox()
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+        public void EnableHitbox()
     {
         foreach( Collider attackCollider in attackColliders)
         {
@@ -30,4 +35,5 @@ public class PlayerHitBoxManager : MonoBehaviour
             attackCollider.enabled = false;
         }
     }
+
 }
